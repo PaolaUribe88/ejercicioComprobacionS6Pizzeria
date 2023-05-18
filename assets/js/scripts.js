20c604524026316092f27ac84f40722a2487d092
PaolaUribe88 <paolauribe88@gmail.com>
2023-05-18 14:16:04 -0400

    Mejora de diseño, y JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,152 +1,142 @@
  //VARIABLES GLOBALES DE LAS BEBIDAS
- var nomBebidas =['Martini', 'Capuccino', 'Latte', 'Mojito'];
- var costosBebida=[2.550, 1.370, 1350, 2.290 ]
- //VARIABLES GLOBALES DE LOS PLATOS
- var nomPlatos = ['Insalata de Riso', 'Insalata ai Cipolli', 'Insalata Caprese']
- var costoPlato = [6.750, 5.990, 8.250 ]
- //______________________BEBIDAS_____________________________
- function verificarMartini(){//MARTINI
-    let elMartini = document.getElementById('martini');
-    let elTituloItem = document.getElementById('cuentaItem');
-    let laCuentaPrecio = document.getElementById(`cuentaPrecio`);
+var nombreBebida=["Martini", "Capuccino", "Latte", "Mojito"];
+var costoBebida=[2.550, 1.370, 1.350, 2.290];
+var arregloIdProductosAdquiridos=[];
+var totalAPagar = 0;
+ //VARIABLES GLOBALES DE LA COMIDA
+var nombreComida=["Insalata de riso", "Insalata ai cipollotti", "Insalata caprese"];
+var costoComida=[6.750, 5.990, 8.250];
+var arregloIdProductosAdquiridosComida=[];
+var totalAPagarComida = 0;
 
-    if(elMartini.checked){
-        let elParrafoTituloNuevo = document.createElement('p');
-        elParrafoTituloNuevo.innerText= `${nomBebidas[0]}`;
-        elTituloItem.appendChild(elParrafoTituloNuevo);
 
-        let elParrafoCuentaNuevo = document.createElement('p');
-        elParrafoCuentaNuevo.innerText =`${costosBebida[0]}`;
-        laCuentaPrecio.appendChild(elParrafoCuentaNuevo);
-    }else{
-        elTituloItem.removeChild(elTituloItem.lastElementChild);
-        laCuentaPrecio.removeChild(laCuentaPrecio.lastElementChild);
+function calcularTotalAPagar(){
+    // se inicializa el total a pagar cada vez que se llame el método para que tenga en cuenta solamente los valores del arreglo
+    totalAPagar = 0;
+    for(let i=0; i<arregloIdProductosAdquiridos.length; i++){
+        let idInformacionBebida = arregloIdProductosAdquiridos[i];
+
+        totalAPagar = totalAPagar + Number(costoBebida[idInformacionBebida]);
+
     }
- }
- function verificarCapuccino(){//CAPUCCINO
-    let elCapuccino = document.getElementById('capuccino');
-    let elTituloItem = document.getElementById('cuentaItem');
-    let laCuentaPrecio = document.getElementById(`cuentaPrecio`);
+    return totalAPagar;
+}
+// CALCULA EL TOTAL A PAGAR EN LA COMIDA
+function calcularTotalAPagarComida(){
+    // se inicializa el total a pagar cada vez que se llame el método para que tenga en cuenta solamente los valores del arreglo
+    totalAPagarComida = 0;
+    for(let i=0; i<arregloIdProductosAdquiridosComida.length; i++){
+        let idInformacionComida = arregloIdProductosAdquiridosComida[i];
 
-    if(elCapuccino.checked){
-        let elParrafoTituloNuevo = document.createElement('p');
-        elParrafoTituloNuevo.innerText= `${nomBebidas[1]}`;
-        elTituloItem.appendChild(elParrafoTituloNuevo);
-
-        let elParrafoCuentaNuevo = document.createElement('p');
-        elParrafoCuentaNuevo.innerText =`${costosBebida[1]}`;
-        laCuentaPrecio.appendChild(elParrafoCuentaNuevo);
-    }else{
-        elTituloItem.removeChild(elTituloItem.lastElementChild);
-        laCuentaPrecio.removeChild(laCuentaPrecio.lastElementChild);
+        totalAPagarComida = totalAPagarComida + Number(costoComida[idInformacionComida]);
     }
- }
- function verificarLatte(){//LATTE
-    let elLatte = document.getElementById('latte');
-    let elTituloItem = document.getElementById('cuentaItem');
-    let laCuentaPrecio = document.getElementById(`cuentaPrecio`);
-    if(elLatte.checked){
-        let elParrafoTituloNuevo = document.createElement('p');
-        elParrafoTituloNuevo.innerText= `${nomBebidas[2]}`;
-        elTituloItem.appendChild(elParrafoTituloNuevo);
+    return totalAPagarComida;
+}
 
-        let elParrafoCuentaNuevo = document.createElement('p');
-        elParrafoCuentaNuevo.innerText =`${costosBebida[2]}`;
-        laCuentaPrecio.appendChild(elParrafoCuentaNuevo);
-    }else{
-        elTituloItem.removeChild(elTituloItem.lastElementChild);
-        laCuentaPrecio.removeChild(laCuentaPrecio.lastElementChild);
-    }
- }
- function verificarMojito(){//MOJITO
-    let elMojito = document.getElementById('mojito');
-    let elTituloItem = document.getElementById('cuentaItem');
-    let laCuentaPrecio = document.getElementById(`cuentaPrecio`);
-    if(elMojito.checked){
-        let elParrafoTituloNuevo = document.createElement('p');
-        elParrafoTituloNuevo.innerText= `${nomBebidas[3]}`;
-        elTituloItem.appendChild(elParrafoTituloNuevo);
+//VERIFICAMOS LOS ELEMENTOS DE LAS BEBIDAS
+function verificarCheck(elemento){      
+    let laCuentaItem = document.getElementById('cuentaItem');
+    let laCuentaPrecio = document.getElementById('cuentaPrecio');
+    let laCajaTotalAPagar = document.getElementById('cajaTotalAPagar');
 
-        let elParrafoCuentaNuevo = document.createElement('p');
-        elParrafoCuentaNuevo.innerText =`${costosBebida[3]}`;
-        laCuentaPrecio.appendChild(elParrafoCuentaNuevo);
-    }else{
-        elTituloItem.removeChild(elTituloItem.lastElementChild);
-        laCuentaPrecio.removeChild(laCuentaPrecio.lastElementChild);
-    }
- }
- //_______________________Platos_________________
- function verificarRiso(){//RISO
-    let elRiso = document.getElementById('riso');
-    let elTituloItem = document.getElementById('cuentaItem');
-    let laCuentaPrecio = document.getElementById(`cuentaPrecio`);
-    if(elRiso.checked){
-        let elParrafoTituloNuevo = document.createElement('p');
-        elParrafoTituloNuevo.innerText= `${nomPlatos[0]}`;
-        elTituloItem.appendChild(elParrafoTituloNuevo);
+    if(elemento.target.checked){
+        arregloIdProductosAdquiridos.push(elemento.target.value);
+        // muestro en la cuenta el último elemento agregado en el arreglo arregloIdProductosAdquiridos
+        let idLaBebidaSeleccionada = arregloIdProductosAdquiridos[arregloIdProductosAdquiridos.length -1];
+        let nombreBebidaSeleccionada = nombreBebida[idLaBebidaSeleccionada];
+        let costoBebidaSeleccionada = costoBebida[idLaBebidaSeleccionada];
 
-        let elParrafoCuentaNuevo = document.createElement('p');
-        elParrafoCuentaNuevo.innerText =`${costoPlato[0]}`;
-        laCuentaPrecio.appendChild(elParrafoCuentaNuevo);
-    }else{
-        elTituloItem.removeChild(elTituloItem.lastElementChild);
-        laCuentaPrecio.removeChild(laCuentaPrecio.lastElementChild);
-    }
- }
- function verificarCipollotti(){//CIPOLLOTTI
-    let elRiso = document.getElementById('cipollotti');
-    let elTituloItem = document.getElementById('cuentaItem');
-    let laCuentaPrecio = document.getElementById(`cuentaPrecio`);
-    if(elRiso.checked){
-        let elParrafoTituloNuevo = document.createElement('p');
-        elParrafoTituloNuevo.innerText= `${nomPlatos[1]}`;
-        elTituloItem.appendChild(elParrafoTituloNuevo);
+        let elParrafoNombreNuevo = document.createElement('p');
+        elParrafoNombreNuevo.innerText =  nombreBebidaSeleccionada;
+        laCuentaItem.appendChild(elParrafoNombreNuevo);
 
-        let elParrafoCuentaNuevo = document.createElement('p');
-        elParrafoCuentaNuevo.innerText =`${costoPlato[1]}`;
-        laCuentaPrecio.appendChild(elParrafoCuentaNuevo);
-    }else{
-        elTituloItem.removeChild(elTituloItem.lastElementChild);
-        laCuentaPrecio.removeChild(laCuentaPrecio.lastElementChild);
+        let elParrafoCostoNuevo = document.createElement('p');
+        elParrafoCostoNuevo.innerText = costoBebidaSeleccionada;
+        laCuentaPrecio.appendChild(elParrafoCostoNuevo);  
     }
- }
- function verificarCaprese(){//CAPRESE
-    let elRiso = document.getElementById('caprese');
-    let elTituloItem = document.getElementById('cuentaItem');
-    let laCuentaPrecio = document.getElementById(`cuentaPrecio`);
-    if(elRiso.checked){
-        let elParrafoTituloNuevo = document.createElement('p');
-        elParrafoTituloNuevo.innerText= `${nomPlatos[2]}`;
-        elTituloItem.appendChild(elParrafoTituloNuevo);
+    else{
+        // identificar el id del producto des-seleccionado
+        let idProductoDeseleccionado = elemento.target.value;
+        // identificamos el indice del elemento desseleccionado en el arreglo arregloIdProductosAdquiridos
+        let indiceElementoDeseleccionado = arregloIdProductosAdquiridos.indexOf(idProductoDeseleccionado);
 
-        let elParrafoCuentaNuevo = document.createElement('p');
-        elParrafoCuentaNuevo.innerText =`${costoPlato[2]}`;
-        laCuentaPrecio.appendChild(elParrafoCuentaNuevo);
-    }else{
-        elTituloItem.removeChild(elTituloItem.lastElementChild);
-        laCuentaPrecio.removeChild(laCuentaPrecio.lastElementChild);
+        // Elimino el elemento desseleccionado del arreglo arregloIdProductosAdquiridos usando splice
+        arregloIdProductosAdquiridos.splice(indiceElementoDeseleccionado,1);
+  
+        // Elimino de la sección de la cuenta el elemento desseleccionado
+        laCuentaItem.removeChild(laCuentaItem.children[indiceElementoDeseleccionado]);
+        laCuentaPrecio.removeChild(laCuentaPrecio.children[indiceElementoDeseleccionado]);
+
     }
- }
+
+    let elTotal = calcularTotalAPagar()
+    laCajaTotalAPagar.innerText = `$`+elTotal.toFixed(3);
+}
+//VERIFICAMOS LOS ELEMENTOS DE LA COMIDA
+function verificarCheckComida(elemento){
+    // CREAMOS EL ELEMENTO LLAMADO POR ID DEL HTML CUENTAITEMCOMIDA
+    let laCuentaItemComida = document.getElementById('cuentaItemComida');
+    // CREAMOS EL ELEMENTO LLAMADO POR ID DEL HTML CUENTAPRECIOCOMIDA
+    let laCuentaPrecioComida = document.getElementById('cuentaPrecioComida');
+    // CREAMOS EL ELEMENTO LLAMADO POR ID DEL HTML CAJATOTALAPAGARCOMIDA
+    let laCajaTotalAPagarComida = document.getElementById('cajaTotalAPagarComida');
+
+    if(elemento.target.checked){
+        arregloIdProductosAdquiridosComida.push(elemento.target.value);
+        // muestro en la cuenta el último elemento agregado en el arreglo arregloIdProductosAdquiridos
+        let idLaComidaSeleccionada = arregloIdProductosAdquiridosComida[arregloIdProductosAdquiridosComida.length -1];
+        let nombreComidaSeleccionada = nombreComida[idLaComidaSeleccionada];
+        let costoComidaSeleccionada = costoComida[idLaComidaSeleccionada];
+
+        let elParrafoNombreNuevoComida = document.createElement('p');
+        elParrafoNombreNuevoComida.innerText =  nombreComidaSeleccionada;
+        laCuentaItemComida.appendChild(elParrafoNombreNuevoComida);
+
+        let elParrafoCostoNuevoComida = document.createElement('p');
+        elParrafoCostoNuevoComida.innerText = costoComidaSeleccionada;
+        laCuentaPrecioComida.appendChild(elParrafoCostoNuevoComida);
+        
+    }
+    else{
+        // identificar el id del producto des-seleccionado
+        let idProductoDeseleccionadoComida = elemento.target.value;
+        // identificamos el indice del elemento desseleccionado en el arreglo arregloIdProductosAdquiridos
+        let indiceElementoDeseleccionadoComida = arregloIdProductosAdquiridosComida.indexOf(idProductoDeseleccionadoComida);
+        // Elimino el elemento desseleccionado del arreglo arregloIdProductosAdquiridos usando splice
+        arregloIdProductosAdquiridosComida.splice(indiceElementoDeseleccionadoComida,1);
+        // Elimino de la sección de la cuenta el elemento desseleccionado
+        laCuentaItemComida.removeChild(laCuentaItemComida.children[indiceElementoDeseleccionadoComida]);
+        laCuentaPrecioComida.removeChild(laCuentaPrecioComida.children[indiceElementoDeseleccionadoComida]);
+
+    }
+
+    let elTotalComida = calcularTotalAPagarComida();
+    laCajaTotalAPagarComida.innerText =`$`+elTotalComida.toFixed(3);
+}
 
 function asignarEventos(){
-     let elMartini = document.getElementById('martini');
-     elMartini.addEventListener('click', verificarMartini);
+    // CREAMOS EL ELEMENTO LLAMADO DEL DOM MARTINI
+    let elCheckMartini = document.getElementById('martini');
+    //LE ASIGNAMOS UN EVENTO QUE AL HACER CLICK HAGA LO QUE EN 
+    //VERIFICARCHECK CREEMOS
+    elCheckMartini.addEventListener('click', verificarCheck);
+    // CREAMOS EL ELEMENTO LLAMADO DEL DOM CAPUCCINO
+    let elCheckCapuccino = document.getElementById('capuccino');
+    elCheckCapuccino.addEventListener('click', verificarCheck);
 
-     let elCapuccino = document.getElementById('capuccino');
-     elCapuccino.addEventListener('click', verificarCapuccino);
+    let elCheckLatte = document.getElementById('latte');
+    elCheckLatte.addEventListener('click', verificarCheck);
 
-     let elLatte = document.getElementById('latte');
-     elLatte.addEventListener('click', verificarLatte);
+    let elCheckMojito = document.getElementById('mojito');
+    elCheckMojito.addEventListener('click', verificarCheck);
 
-     let elMojito = document.getElementById('mojito');
-     elMojito.addEventListener('click',verificarMojito);
-     //__________Platos______
-     let elRiso = document.getElementById('riso');
-     elRiso.addEventListener('click', verificarRiso);
+    let elCheckRiso = document.getElementById('riso');
+    elCheckRiso.addEventListener('click', verificarCheckComida);
 
-     let elCipollotti = document.getElementById('cipollotti');
-     elCipollotti.addEventListener('click', verificarCipollotti);
+    let elCheckCipollotti = document.getElementById('cipollotti');
+    elCheckCipollotti.addEventListener('click', verificarCheckComida);
 
-     let elCaprese = document.getElementById('caprese');
-     elCaprese.addEventListener('click', verificarCaprese);
+    let elCheckCaprese = document.getElementById('caprese');
+    elCheckCaprese.addEventListener('click', verificarCheckComida);
+    
 }
